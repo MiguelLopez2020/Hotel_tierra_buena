9fa620dbe6c435cce18fa2bb61fa8602c5d0864c
MiguelLopez2020 <miguel_angel_lopez_s@hotmail.com>
2021-04-26 22:32:40 -0500

    logica de fechas

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,1 +1,24 @@
-Swal.fire('Any fool can use a computer')
+function consulta() {
+    var x = document.getElementById("Fecha1")
+    var y = document.getElementById("Fecha2")
+    var z = new Date();
+
+    var añoToday = z.getFullYear();
+    var mesToday = z.getMonth();
+    var diaToday = z.getDate();
+
+    var añoLle = x.getFullYear();
+    var mesLle = x.getMonth();
+    var diaLle = x.getDate();
+
+    var añoSal = y.getFullYear();
+    var mesSal = y.getMonth();
+    var diaSal = y.getDate();
+
+    if (añoToday < añoLle && mesToday < mesLle && diaToday < diaLle) {
+document
+    }
+
+}
+
+
