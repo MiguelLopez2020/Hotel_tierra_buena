a1fdde852b10edf737f9e2914de87ef9653d21ce
MiguelLopez2020 <miguel_angel_lopez_s@hotmail.com>
2021-04-27 20:07:44 -0500

    carga de logica

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,24 +1,43 @@
 function consulta() {
-    var x = document.getElementById("Fecha1")
-    var y = document.getElementById("Fecha2")
+    var x = document.getElementById("Fecha1").value
+    var y = document.getElementById("Fecha2").value
     var z = new Date();
 
-    var añoToday = z.getFullYear();
-    var mesToday = z.getMonth();
-    var diaToday = z.getDate();
+    //fecha de llegada
+    var x2 = x.split(" ")[0]
 
-    var añoLle = x.getFullYear();
-    var mesLle = x.getMonth();
-    var diaLle = x.getDate();
+    var x3 = x2.substring(0, 4)//año llegada
+    var x4 = x2.substring(5, 7)//mes llegada
+    var x5 = x2.substring(8, 10)//dia llegada
 
-    var añoSal = y.getFullYear();
-    var mesSal = y.getMonth();
-    var diaSal = y.getDate();
+    //fecha de salida
+    var y2 = y.split(" ")[0]
 
-    if (añoToday < añoLle && mesToday < mesLle && diaToday < diaLle) {
-document
+    var y3 = y2.substring(0, 4)//año salida
+    var y4 = y2.substring(5, 7)//mes salida
+    var y5 = y2.substring(8, 10)//dia salida
+
+    //fecha del dia de hoy
+    var anoToday = z.getFullYear();//año actual
+    var mesToday = z.getMonth();//mes actual
+    var diaToday = z.getDate();//dia actual
+
+
+
+    if (x3 <= anoToday || x4 <= mesToday || x5 <= diaToday && y3 === "" && y4 === "" && y5 === "") {
+        console.log("Revisa si estas reservando en fechas anteriores o si no tienes seleccionada una fecha de salida")
+
+    } else if (x3 === "" && x4 === "" && x5 === "" && y3 === "" && y4 === "" && y5 === "") {
+        console.log("no has seleccionado fechas ")
+
+    } else if (x3 === "" && x4 === "" && x5 === "" && y3 != "" && y4 != "" && y5 != "") {
+        console.log("no has seleccionado una fecha de llegada")
+
+    } else if (x3 != "" && x4 != "" && x5 != "" && y3 === "" && y4 === "" && y5 === "") {
+        console.log("no has seleccionado una fecha de salida")
     }
 
 }
+
 
 
