5c5c9ef99965932f42f0edff1dfe0e6611a60b83
carlosandresmm <82244181+carlosandresmm@users.noreply.github.com>
2021-05-03 15:24:09 -0500

    gif nosotros

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -6,9 +6,52 @@ function registro(){
     var password_1 = document.getElementById("recipient-name4").value
      
     if(nombre_1!=""&&correo_1!="" &&apellido_1!="" &&password_1!=""){
-        alert("Muchas gracias, te enviamos la confirmacion a tu correo!")
-
+        swal.fire({
+            title: "¡Registro exitoso!",
+            text:"Enviamos la confirmacion a su correo electronico",
+            icon:"success"
+        })
     }else{
-        alert("Se debe diligenciar todos los campos")
-    }    return
+        swal.fire({
+            
+            title: "¡Lo sentimos!",
+            text: "El formulario no puede tener espacios en blanco",
+            icon: "error"
+            
+        }
+            
+        )
+    }
+return
+}
+
+
+function contactar(){
+    var nombre_1 = document.getElementById("recipient-name1").value
+    var apellido_1 = document.getElementById("recipient-name2").value
+    var correo_1 = document.getElementById("recipient-name3").value
+    
+    var texto_1 = document.getElementById("recipient-name4").value
+     
+    if(nombre_1!=""&&correo_1!="" &&apellido_1!="" &&texto_1!=""){
+        swal.fire({
+            title: "¡Muchas gracias!",
+            text:"Muy pronto nos pondremos en contacto",
+            icon:"success"
+        })
+
+
+        
+    }else{
+        swal.fire({
+            
+            title: "¡Lo sentimos!",
+            text: "El formulario no puede tener espacios en blanco",
+            icon: "error"
+            
+        }
+            
+        )
+    }
+return
 }
